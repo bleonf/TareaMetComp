7d4b5972aaff7531e7103eeafd8197a8f69a4ad2
bleonf <42364547+bleonf@users.noreply.github.com>
2018-11-18 21:52:42 -0500

    Add files via upload

--- ODE.cpp
+++ ODE.cpp
@@ -3,7 +3,7 @@
 using std::cout;
 
 const float  PI_F=3.1415926;
-
+//const float pasos=500;
 float vel(float h, float n,float vel,float nmenos, float g);
 float primerpasovelx(float angulo, float h);
 float primerpasovely(float angulo, float h);
@@ -16,36 +16,163 @@ float rads(float angulo);
 
 //ecu final: x_n+1=-g-(abs(x_n)*x_n)(2h)+x_n-1
 
-float posx[50000];
-float posy[50000];
-float velx[50000];
-float vely[50000];
+float posx[500];
+float posy[500];
+float velx[500];
+float vely[500];
+float tiempo[500];
 int main()
 {
 	float g=10.0;
 	float c=0.2;
 	float m=0.2;
 	
-	float h=0.00001;
-	int pasos=50000;
+	float h=0.001;
+	float pasos=500;
 	//tiempo final=h*pasos
-	
-	velx[0]=300*cos(rads(45));
-	vely[0]=300*sin(rads(45));
-	velx[1]=velx[0]+((-0.0-(abs(velx[0])*(velx[0])))*h);
-	vely[1]=vely[0]+((-10-(abs(vely[0])*(vely[0])))*h);
+	for(int i=0;i<pasos;i++)
+		tiempo[i]=h*i;
+		
+//////////////////////45	
+	velx[0]=300*cos(rads(45.0));
+	vely[0]=300*sin(rads(45.0));
+	velx[1]=velx[0]-(((abs(velx[0])*(velx[0])))*h);
+	vely[1]=vely[0]-((10.0+(abs(vely[0])*(vely[0])))*h);//10 de gravedad en Y
 	
 	posx[0]=0;
 	posy[0]=0;
-	cout<<velx[0]<<" "<<vely[0]<<" "<<posx[1]<<" "<<posy[1]<<"\n";
+	cout<<velx[0]<<" "<<vely[0]<<" "<<posx[1]<<" "<<posy[1]<<" "<<tiempo[1]<<"\n";
 	for(int i=2;i<pasos;i++)
 		{
 		velx[i]=vel(h,velx[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),velx[i-2],0.0);
 		vely[i]=vel(h,vely[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),vely[i-2],g);
 		posx[i-1]=posx[i-2]+(velx[i-2]*h);
 		posy[i-1]=posy[i-2]+(vely[i-2]*h);
-		cout<<velx[i-1]<<" "<<vely[i-1]<<" "<<posx[i-2]<<" "<<posy[i-2]<<"\n";
+		cout<<velx[i-1]<<" "<<vely[i-1]<<" "<<posx[i-2]<<" "<<posy[i-2]<<" "<<tiempo[i-1]<<"\n";
 		}
+////////////////////////10	
+	velx[0]=300*cos(rads(10.0));
+	vely[0]=300*sin(rads(10.0));
+	velx[1]=velx[0]-(((abs(velx[0])*(velx[0])))*h);
+	vely[1]=vely[0]-((10.0+(abs(vely[0])*(vely[0])))*h);//10 de gravedad en Y
+	
+	posx[0]=0;
+	posy[0]=0;
+	cout<<velx[0]<<" "<<vely[0]<<" "<<posx[0]<<" "<<posy[0]<<" "<<tiempo[0]<<"\n";
+	for(int i=2;i<pasos;i++)
+		{
+		velx[i]=vel(h,velx[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),-1*velx[i-2],0.0);
+		vely[i]=vel(h,vely[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),-1*vely[i-2],g);
+		posx[i-1]=posx[i-2]+(velx[i-2]*h);
+		posy[i-1]=posy[i-2]+(vely[i-2]*h);
+		cout<<velx[i-1]<<" "<<vely[i-1]<<" "<<posx[i-1]<<" "<<posy[i-1]<<" "<<tiempo[i-1]<<"\n";
+		}
+////////////////////////20	
+	velx[0]=300*cos(rads(45.0));
+	vely[0]=300*sin(rads(45.0));
+	velx[1]=velx[0]-(((abs(velx[0])*(velx[0])))*h);
+	vely[1]=vely[0]-((10.0+(abs(vely[0])*(vely[0])))*h);//10 de gravedad en Y
+	
+	posx[0]=0;
+	posy[0]=0;
+	cout<<velx[0]<<" "<<vely[0]<<" "<<posx[0]<<" "<<posy[0]<<" "<<tiempo[0]<<"\n";
+	for(int i=2;i<pasos;i++)
+		{
+		velx[i]=vel(h,velx[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),velx[i-2],0.0);
+		vely[i]=vel(h,vely[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),vely[i-2],g);
+		posx[i-1]=posx[i-2]+(velx[i-2]*h);
+		posy[i-1]=posy[i-2]+(vely[i-2]*h);
+		cout<<velx[i-1]<<" "<<vely[i-1]<<" "<<posx[i-1]<<" "<<posy[i-1]<<" "<<tiempo[i-1]<<"\n";
+		}
+////////////////////////30	
+	velx[0]=300*cos(rads(45));
+	vely[0]=300*sin(rads(45));
+	velx[1]=velx[0]-(((abs(velx[0])*(velx[0])))*h);
+	vely[1]=vely[0]-((10.0+(abs(vely[0])*(vely[0])))*h);//10 de gravedad en Y
+	
+	posx[0]=0;
+	posy[0]=0;
+	cout<<velx[0]<<" "<<vely[0]<<" "<<posx[0]<<" "<<posy[0]<<" "<<tiempo[0]<<"\n";
+	for(int i=2;i<pasos;i++)
+		{
+		velx[i]=vel(h,velx[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),velx[i-2],0.0);
+		vely[i]=vel(h,vely[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),vely[i-2],g);
+		posx[i-1]=posx[i-2]+(velx[i-2]*h);
+		posy[i-1]=posy[i-2]+(vely[i-2]*h);
+		cout<<velx[i-1]<<" "<<vely[i-1]<<" "<<posx[i-1]<<" "<<posy[i-1]<<" "<<tiempo[i-1]<<"\n";
+		}
+////////////////////////40	
+	velx[0]=300*cos(rads(45));
+	vely[0]=300*sin(rads(45));
+	velx[1]=velx[0]-(((abs(velx[0])*(velx[0])))*h);
+	vely[1]=vely[0]-((10.0+(abs(vely[0])*(vely[0])))*h);//10 de gravedad en Y
+	
+	posx[0]=0;
+	posy[0]=0;
+	cout<<velx[0]<<" "<<vely[0]<<" "<<posx[0]<<" "<<posy[0]<<" "<<tiempo[0]<<"\n";
+	for(int i=2;i<pasos;i++)
+		{
+		velx[i]=vel(h,velx[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),velx[i-2],0.0);
+		vely[i]=vel(h,vely[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),vely[i-2],g);
+		posx[i-1]=posx[i-2]+(velx[i-2]*h);
+		posy[i-1]=posy[i-2]+(vely[i-2]*h);
+		cout<<velx[i-1]<<" "<<vely[i-1]<<" "<<posx[i-1]<<" "<<posy[i-1]<<" "<<tiempo[i-1]<<"\n";
+		}
+////////////////////////50	
+	velx[0]=300*cos(rads(45));
+	vely[0]=300*sin(rads(45));
+	velx[1]=velx[0]-(((abs(velx[0])*(velx[0])))*h);
+	vely[1]=vely[0]-((10.0+(abs(vely[0])*(vely[0])))*h);//10 de gravedad en Y
+	
+	posx[0]=0;
+	posy[0]=0;
+	cout<<velx[0]<<" "<<vely[0]<<" "<<posx[0]<<" "<<posy[0]<<" "<<tiempo[0]<<"\n";
+	for(int i=2;i<pasos;i++)
+		{
+		velx[i]=vel(h,velx[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),velx[i-2],0.0);
+		vely[i]=vel(h,vely[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),vely[i-2],g);
+		posx[i-1]=posx[i-2]+(velx[i-2]*h);
+		posy[i-1]=posy[i-2]+(vely[i-2]*h);
+		cout<<velx[i-1]<<" "<<vely[i-1]<<" "<<posx[i-1]<<" "<<posy[i-1]<<" "<<tiempo[i-1]<<"\n";
+		}
+
+////////////////////////60	
+	velx[0]=300*cos(rads(60.0));
+	vely[0]=300*sin(rads(60.0));
+	velx[1]=velx[0]-(((abs(velx[0])*(velx[0])))*h);
+	vely[1]=vely[0]-((10.0+(abs(vely[0])*(vely[0])))*h);//10 de gravedad en Y
+	
+	posx[0]=0;
+	posy[0]=0;
+	cout<<velx[0]<<" "<<vely[0]<<" "<<posx[0]<<" "<<posy[0]<<" "<<tiempo[0]<<"\n";
+	for(int i=2;i<pasos;i++)
+		{
+		velx[i]=vel(h,velx[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),velx[i-2],0.0);
+		vely[i]=vel(h,vely[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),vely[i-2],g);
+		posx[i-1]=posx[i-2]+(velx[i-2]*h);
+		posy[i-1]=posy[i-2]+(vely[i-2]*h);
+		cout<<velx[i-1]<<" "<<vely[i-1]<<" "<<posx[i-1]<<" "<<posy[i-1]<<" "<<tiempo[i-1]<<"\n";
+		}
+
+////////////////////////70	
+	velx[0]=300*cos(rads(70.0));
+	vely[0]=300*sin(rads(70.0));
+	velx[1]=velx[0]-(((abs(velx[0])*(velx[0])))*h);
+	vely[1]=vely[0]-((10.0+(abs(vely[0])*(vely[0])))*h);//10 de gravedad en Y
+	
+	posx[0]=0;
+	posy[0]=0;
+	cout<<velx[0]<<" "<<vely[0]<<" "<<posx[0]<<" "<<posy[0]<<" "<<tiempo[0]<<"\n";
+	for(int i=2;i<pasos;i++)
+		{
+		velx[i]=vel(h,velx[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),velx[i-2],0.0);
+		vely[i]=vel(h,vely[i-1],pow(((velx[i-1]*velx[i-1])+(vely[i-1]*vely[i-1])),0.5),vely[i-2],g);
+		posx[i-1]=posx[i-2]+(velx[i-2]*h);
+		posy[i-1]=posy[i-2]+(vely[i-2]*h);
+		cout<<velx[i-1]<<" "<<vely[i-1]<<" "<<posx[i-1]<<" "<<posy[i-1]<<" "<<tiempo[i-1]<<"\n";
+		}
+	
+
 		
 		
 	
--- PDE.cpp
+++ PDE.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 using std::cout;
 
+float pos[51][51];//para cada borde con una interseccion en todo el centro de la varilla
 
 //varilla de 0.5*0.5
 int main()
 {
-float d=0.1;//diametro
+float d=0.1;//diametro o 10 cuadrados
 float cp=820.0;//C_p
 float k=1.62;//conductividad termica
 float p=2.71;//densidad
 float v=k/(cp*p);//coeficiente de difusion
-float T=100;//temperatura=cte
+float T=100.0;//temperatura=cte
+
+
+//condicion que utilizaremos ya que la varilla se mantiene a 100 grados
+for (int i=0;i<51;i++)
+	{
+	for (int j=0;j<51;j++)
+		{
+		if (pow((pow((i-26),2)+pow((j-26),2)),0.5)<10)
+			{
+			pos[i][j]=T;
+			}
+		}
+	}
 
 return 0;
 }
